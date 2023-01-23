8fd2244c1f27134529d200f251d7ef95381e3d7c
JW30 <wagnerxjonathan@gmail.com>
2023-01-23 12:36:13 +0100

    Add second part of section 5

---
+++ assignments/assignment03/app.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/users", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "views", "users.html"));
+})
+
+app.listen(4040);
